cf6777b2d32b9061b8a9c2c540ec334833d02d08
ecspresso <emile@ecspresso.com>
2018-09-13 15:06:30 +0200

    Windows install script

---
+++ install.ps1
@@ -0,0 +1,16 @@
+﻿$file = "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\DontSit.jar"
+$startup = "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+
+while(Test-Path $file) {
+    rm $file
+    if(!$?) {
+        Write-Host "Program is still running. Close program and press any key to continue."
+        $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+    } else {
+        break
+    }
+}
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest $((Invoke-RestMethod -Uri https://api.github.com/repos/ecspresso/DontSit/releases/latest).Assets.browser_download_url | Where {$_ -like "*DontSit.jar" }) -OutFile $($startup + "DontSit.jar")
+Invoke-Item $startup
